5dab9824dfeeeba144eb3d4ee318136d2f931d89
sanjib00 <sanjib_1@hotmail.com>
2014-08-20 10:04:38 +0600

    Understanding assertSame Function

--- app/tests/controllers/practicetest-2.php
+++ app/tests/controllers/practicetest-2.php
@@ -5,7 +5,12 @@ class PracticeTest extends PHPUnit_Framework_TestCase {
     {
         $val = null;
         $this->assertEquals(0, $val);
+        $val = null;
+        $this->assertSame(null, $val);
 
-       
+        $greeting = 'Hello, World.';
+        $this->assertEquals('Hello, World.', $greeting);
+
+
     }
 }
